ca347ff4104f1bb04e2af25ef74d7924d521a861
dlace51 <142068671+dlace51@users.noreply.github.com>
2024-10-18 23:25:32 -0500

    changed models again

--- shop/models.py
+++ shop/models.py
@@ -3,10 +3,8 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200,
-                            db_index=True)
-    slug = models.SlugField(max_length=200,
-                            unique=True)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -17,18 +15,14 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-            return reverse('shop:product_list_by_category',
-                           args=[self.slug])
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category,
-                                 related_name='products',
-                                 on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
-                              blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -44,5 +38,30 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail',
-                           args=[self.id, self.slug])
+        return reverse('shop:product_detail', args=[self.id, self.slug])
+
+
+# Suit model for your specific project use case
+class Suit(models.Model):
+    category = models.ForeignKey(Category, related_name='suits', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    size = models.CharField(max_length=50)  # Added based on ERD
+    color = models.CharField(max_length=50)  # Added based on ERD
+    rental_price = models.DecimalField(max_digits=10, decimal_places=2)  # Rental price instead of standard price
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='suits/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)  # For suit details like material, etc.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:suit_detail', args=[self.slug])
+
