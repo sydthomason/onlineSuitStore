1e36522ff513e34446f63e9930d69f215a6e90b7
dlace51 <142068671+dlace51@users.noreply.github.com>
2024-10-18 23:13:24 -0500

    added suit model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Suit(models.Model):
+    suit_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    rental_price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability_status = models.BooleanField(default=True)  # True means available for rent/buy
+
+    def __str__(self):
+        return self.name
